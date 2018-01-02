696a9d39119a8539f4bce95479126b7b6e96d216
danokenka <danokenka@users.noreply.github.com>
2018-01-01 21:09:07 -0500

    ionic and angular

---
+++ src/shared/dish.ts
@@ -0,0 +1,14 @@
+import {Comment } from './comment';
+
+export interface Dish {
+    id: number;
+    name: string;
+    image: string;
+    category: string;
+    label: string;
+    price: string;
+    featured: string;
+    description: string;
+    comments: Comment[];
+
+}
